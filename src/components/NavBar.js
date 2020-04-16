149fdde1535e2dd02c196270de1b4f9b9cdf0731
Simon Lee <simonhlee97@hotmail.com>
2020-04-16 17:29:12 +0900

    created a homepage and scss

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div>
+    <ul class="nav">
+	   <li><a href="https://github.com/">About</a></li>
+	   <li><a href="https://github.com/">Home</a></li>
+	   <li><a href="https://github.com/">ChallengeTrends</a></li>
+	</ul>
+  </div>
+);
+
+export default NavBar
