7bc46253a60c44849d2d94dff7908af7c686d50b
Simon Lee <simonhlee97@hotmail.com>
2020-04-17 19:17:40 +0900

    Firebase Auth and Firestore

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,8 +52,7 @@ class SignIn extends React.Component {
 					/>
 					<div className='buttons'>
 						<CustomButton type='submit'>Sign in</CustomButton>
-
-						<CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
 					</div>
 				</form>
 			</div>
