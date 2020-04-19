0add9a18e98414b68c3de0c4ec315c3e270c4696
Simon Lee <simonhlee97@hotmail.com>
2020-04-19 19:40:34 +0900

    cart.selectors to keep track of item count without re-rendering

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = ({cartItems}) => {
-  return (
-    <div className='cart-dropdown'>
-    	<div className='cart-items'>
-    		{cartItems.map(cartItem => (
-    			<CartItem key={cartItem.id} item={cartItem} />
-    			))
-    		}
-    	</div>
-    	<CustomButton>GO TO CHECKOUT</CustomButton>
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ({ cartItems }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-  )
-}
-const mapStateToProps = ({cart: {cartItems }}) => ({
-	cartItems
-}); 
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
 
-
-
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
+});
 
 export default connect(mapStateToProps)(CartDropdown);
